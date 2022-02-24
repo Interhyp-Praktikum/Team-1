6a6dc3327736efeced4f26cb8b81a78989f25caa
maidi29 <mareike.haug@web.de>
2022-02-24 20:59:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var outfit1 = {
   jahreszeit: "Frühling",
   material: "Baumwolle",
   aktivität: "Spazieren",
-  farbe: ["Schwarz", "Weiß"]
+  farbe: ["Schwarz", "Weiß"],
+  suchbegriffe: ["Streifen", "Rüschen"],
+  beschreibung: "Dies ist ein Schönes Sommeroutfit:"
 };
 var outfit2 = {
   bild:
@@ -12,7 +14,10 @@ var outfit2 = {
   jahreszeit: "Frühling",
   material: "Baumwolle",
   aktivität: "Outdoor Sport",
-  farbe: ["Weiß"]
+  farbe: ["Weiß"],
+  suchbegriffe: ["Bauchfrei"],
+  beschreibung:
+    "Dies ist ein cooles, bauchfreies Trainingsoutfit für den Frühling:"
 };
 
 var outfit3 = {
@@ -20,24 +25,316 @@ var outfit3 = {
   jahreszeit: "Herbst",
   material: "Polyester",
   aktivität: "Outdoor Sport",
-  farbe: ["Schwarz", "Weiß"]
+  farbe: ["Blau", "Rot"],
+  suchbegriffe: ["Jogginghose", "Trainingsanzug"],
+  beschreibung:
+    "Das ist ein blauer Trainingsanzug von Adidas mit orangenen Streifen an der Hose:"
 };
 
+var outfit4 = {
+  bild:
+    "https://img01.ztat.net/article/spp-media-p1/12041618aa1d43b5b61dfc25db3853c8/d27cd34bc5c5422fb5ea992dda95aa82.jpg?imwidth=762",
+  jahreszeit: "Herbst",
+  material: "Polyester",
+  aktivität: "Outdoor Sport",
+  farbe: ["Blau"],
+  suchbegriffe: ["Streifen", "weiblich", "adidas"],
+  beschreibung:
+    "Das ist ein longsleeve Pullover Outfit mit einer weiß gestreifte blaue Trainingshose:"
+};
+var outfit5 = {
+  bild:
+    "https://ridestore.imgix.net/catalog/product//E/9/E9513_200.jpg?fit=max&w=736&q=80&dpr=1&usm=15&fm=avif",
+  jahreszeit: "Winter",
+  material: "Wasserdicht",
+  aktivität: "Skifahren",
+  farbe: ["Rot"],
+  suchbegriffe: ["Red Bull Helm"],
+  beschreibung: "Hier sehen wir einen roten Skihelm von Redbull:"
+};
+var outfit6 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x640/202003/ngnppt1583233259366.jpg?fmt=webp&v=1",
+  jahreszeit: "Frühling",
+  material: "Baumwolle",
+  aktivität: "Spazieren",
+  farbe: ["Gelb"],
+  suchbegriffe: ["gemustert"],
+  beschreibung: "Dies ist camouflage gemustertes Outfit:"
+};
+var outfit7 = {
+  bild:
+    "https://ridestore.imgix.net/catalog/product//F0702_01_Xh9mQq5.jpg?fit=max&w=736&q=80&dpr=1&usm=15&fm=avif",
+  jahreszeit: "Winter",
+  material: "Wasserdicht",
+  aktivität: "Skifahren",
+  farbe: ["Weiß"],
+  suchbegriffe: ["Montac", "Windbreaker"],
+  beschreibung:
+    "Eine coole, weiße Skijacke, die einen im Schnee nahezu unerkennbar macht:"
+};
+var outfit8 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x640/201912/zuybkx1577705266069.jpg?fmt=webp&v=1",
+  jahreszeit: "Frühling",
+  material: "Baumwolle",
+  aktivität: "Spazieren",
+  farbe: ["Grau"],
+  suchbegriffe: ["kurzes Oberteil"],
+  beschreibung: "ein full-grey Outfit für den Spaziergang:"
+};
+var outfit9 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x640/202012/pkmaim1608617121900.jpg?fmt=webp&v=1",
+  jahreszeit: "Sommer",
+  material: "Baumwolle",
+  aktivität: "Outdoor Sport",
+  farbe: ["Weiß"],
+  suchbegriffe: ["optische Täuschung"],
+  beschreibung:
+    "Eine optische Täuschung von einem stark trainiertem Oberkörper, welcher das Shirt aufzureißen scheint."
+};
+var outfit10 = {
+  bild:
+    "https://s.w-x.co/util/image/w/MicrosoftTeams-image_1.png?crop=16:9&width=980&format=pjpg&auto=webp&quality=60",
+  jahreszeit: "Frühling",
+  material: "Polyester",
+  aktivität: "Wandern",
+  farbe: ["Blau"],
+  suchbegriffe: ["Berge"],
+  beschreibung: " ein 100% Polyester Wander-Outfit:"
+};
+var outfit11 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x640/202106/bps/product/inc/qvjhno1622613160864.jpg?fmt=webp&v=1",
+  jahreszeit: "Frühling",
+  material: "Wasserdicht",
+  aktivität: "Spazieren",
+  farbe: ["Weiß"],
+  suchbegriffe: ["Maske"],
+  beschreibung:
+    "Das ist eine coole Maske für den Corona-Alltag mit einem Bild von Kevin-Allein zu Haus darauf:"
+};
+var outfit12 = {
+  bild:
+    "https://img01.ztat.net/article/spp-media-p1/e8902a58b4ae32cd8759aec37f7c9402/0c6fff4425c241a9bee5f1736f7d03ae.jpg?imwidth=156",
+  jahreszeit: "Sommer",
+  material: "Polyester",
+  aktivität: "Spazieren",
+  farbe: ["Weiß"],
+  suchbegriffe: ["Blumen-Muster", "ärmelfrei"],
+  beschreibung: "ein weißes Kleid mit einem Blumen-Muster:"
+};
+var outfit13 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x853/202112/bps/product/inc/ngnlzw1639724123871.jpg?fmt=webp&v=1",
+  jahreszeit: "Frühling",
+  material: "Baumwolle",
+  aktivität: "Outdoor Sport",
+  farbe: ["Grau"],
+  suchbegriffe: ["jogging anzug"],
+  beschreibung:
+    "Ein cooles Outdoor-Trainingsoutfit für weibliche Teenager in Grau:"
+};
+var outfit14 = {
+  bild:
+    "https://i.pinimg.com/564x/0a/73/71/0a73713a0f8df27d378c99bf6187aa78.jpg",
+  jahreszeit: "Sommer",
+  material: "Polyester",
+  aktivität: "Outdoor Sport",
+  farbe: ["Schwarz"],
+  suchbegriffe: ["stramm"],
+  beschreibung: "Dies ist ein cooles schwarzes Sommer-Outfit:"
+};
+var outfit15 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x853/202109/bps/product/inc/doychl1630477987986.jpg?fmt=webp&v=1",
+  jahreszeit: "Sommer",
+  material: "Baumwolle",
+  aktivität: "Spazieren",
+  farbe: ["Grau"],
+  suchbegriffe: ["3d Täuschung"],
+  beschreibung:
+    "Hier sieht man erneut eine optische Täuschung von einem durchtrainiertem Mann, wo es scheint, als hätte dieser sein Shirt hochgezogen. Das ist ebenfalls ein cooles Outfit für den Sommer. "
+};
+var outfit16 = {
+  bild:
+    "https://cdn.shopify.com/s/files/1/0278/2350/5460/products/592A2553Teddy_Ecomm_08e3c364-205a-4adb-abfe-8f7d81961f80_820x.jpg",
+  jahreszeit: "Sommer",
+  material: "Baumwolle",
+  aktivität: "Couch",
+  farbe: ["Grau"],
+  suchbegriffe: ["bauchfrei", "flauschig"],
+  beschreibung: "ein lässiges Outfit für zu Hause:"
+};
+var outfit17 = {
+  bild:
+    "https://litb-cgis.rightinthebox.com/images/640x853/202108/bps/product/inc/yhrjfe1630393723005.jpg?fmt=webp&v=1",
+  jahreszeit: "Sommer",
+  material: "Baumwolle",
+  aktivität: "Event",
+  farbe: ["Blau"],
+  suchbegriffe: ["Event-Kleid"],
+  beschreibung: "Das ist ein tolles, blaues Kleid, z.B. für ein On-Stage-Event:"
+};
+var outfit18 = {
+  bild: "https://cdn.hummelonlineshop-muenchen.de/full/H205870-2001.jpg",
+  jahreszeit: "Sommer",
+  material: "Polyester",
+  aktivität: "Indoor Sport",
+  farbe: ["Schwarz"],
+  suchbegriffe: ["Handball-Trikot"],
+  beschreibung: "ein tolles Hallen-Outfit für Handball, Fußball etc. :"
+};
+var outfit19 = {
+  bild:
+    "https://cdn.oceansapart.com/de/wp-content/uploads/sites/2/2019/04/0001_Daylight-Set-1.jpg",
+  jahreszeit: "Sommer",
+  material: "Seide",
+  aktivität: "Outdoor Sport",
+  farbe: ["Rose"],
+  suchbegriffe: ["Strand"],
+  beschreibung:
+    "Ein hübsches, rosa Outfit, zum Laufen oder für das Beach-Volleyballspiel am Strand:"
+};
+
+var outfit20 = {
+  bild:
+    "https://m.media-amazon.com/images/I/71nN+4VKkwL._AC_SY606._SX._UX._SY._UY_.jpg",
+  jahreszeit: "Winter",
+  material: "Baumwolle",
+  aktivität: "Couch",
+  farbe: ["Rose"],
+  suchbegriffe: ["Sprüche"],
+  beschreibung: "Ein gemütlicher Pullover mit einer weißen Aufschrift"
+};
+
+var outfit21 = {
+  bild:
+    "https://m.media-amazon.com/images/I/71oQUT8Tq9L._AC_UY741_.jpghttps://m.media-amazon.com/images/I/71oQUT8Tq9L._AC_UY741_.jpghttps://m.media-amazon.com/images/I/71oQUT8Tq9L._AC_UY741_.jpg",
+  jahreszeit: "Herbst",
+  material: "Baumwolle",
+  aktivität: "Spazieren",
+  farbe: ["Schwarz"],
+  suchbegriffe: ["Jeans"],
+  beschreibung:
+    "Eine coole, lässige Jeans für weibliche Teenager für den Alltag im Sommer:"
+};
+
+var outfit22 = {
+  bild:
+    "https://m.media-amazon.com/images/I/715nKJypLJL._AC_SY606._SX._UX._SY._UY_.jpg",
+  jahreszeit: "Sommer",
+  material: "Baumwolle",
+  aktivität: "Spazieren",
+  farbe: ["Weiß"],
+  suchbegriffe: ["Sommer-Top"],
+  beschreibung: "Bauchfreies Sommer-Top in Weiß für Teenager"
+};
+
+var outfits = [
+  outfit1,
+  outfit2,
+  outfit3,
+  outfit4,
+  outfit5,
+  outfit6,
+  outfit7,
+  outfit8,
+  outfit9,
+  outfit10,
+  outfit11,
+  outfit12,
+  outfit13,
+  outfit14,
+  outfit15,
+  outfit16,
+  outfit17,
+  outfit18,
+  outfit19,
+  outfit20,
+  outfit21,
+  outfit22
+];
+
 window.sucheErgebnisse = function () {
+  var ergebnisse = document.querySelector("#ergebnisse");
+  ergebnisse.innerHTML = "";
+  var AnzahlderErgebnisse = document.querySelector("#Anzahl");
   var wertAusDemSuchFeld = document.querySelector(".suche").value;
   var wertAusDemJahreszeitenFeld = document.querySelector("select.Jahreszeiten")
     .value;
   var wertAusDemAktivitätenFeld = document.querySelector("select.Aktivität")
     .value;
   var wertAusDemMaterialFeld = document.querySelector("select.Material").value;
+  var WertAusDemFarbenFeld = document.querySelector(".Farbe").value;
 
-  console.log(wertAusDemJahreszeitenFeld);
-  console.log(wertAusDemAktivitätenFeld);
-  console.log(wertAusDemSuchFeld);
-  console.log(wertAusDemMaterialFeld);
-  var WerteAusDemFarbenFeld = document.querySelector(".Farbe").value;
-  console.log(WerteAusDemFarbenFeld);
+  var gefilterteOutfits = outfits.filter(function (outfit) {
+    var farbeEntsprichtAuswahlOderNochNichtGewählt =
+      outfit.farbe.includes(WertAusDemFarbenFeld) ||
+      WertAusDemFarbenFeld === "";
+    var jahreszeitEntsprichtAuswahlOderNochNichtGewählt =
+      outfit.jahreszeit === wertAusDemJahreszeitenFeld ||
+      wertAusDemJahreszeitenFeld === "";
+    var aktivitätEntsprichtAuswahlOderNochNichtGewählt =
+      outfit.aktivität === wertAusDemAktivitätenFeld ||
+      wertAusDemAktivitätenFeld === "";
+    var materialEntsprichtAuswahlOderNochNichtGewählt =
+      outfit.material === wertAusDemMaterialFeld ||
+      wertAusDemMaterialFeld === "";
+    var beschreibungEntsprichtAuswahlOderNochNichtGewählt =
+      outfit.suchbegriffe.includes(wertAusDemSuchFeld) ||
+      wertAusDemSuchFeld === "";
+    return (
+      farbeEntsprichtAuswahlOderNochNichtGewählt &&
+      aktivitätEntsprichtAuswahlOderNochNichtGewählt &&
+      materialEntsprichtAuswahlOderNochNichtGewählt &&
+      jahreszeitEntsprichtAuswahlOderNochNichtGewählt &&
+      beschreibungEntsprichtAuswahlOderNochNichtGewählt
+    );
+  });
 
-  var ergebnisse = document.querySelector("#ergebnisse");
-  ergebnisse.innerHTML = '<img width="300px" src="' + outfit1.bild + '"></img>';
+  AnzahlderErgebnisse.innerText = gefilterteOutfits.length;
+
+  for (var index = 0; index < gefilterteOutfits.length; index++) {
+    ergebnisse.innerHTML += `
+    <div class="ergebnis">
+      <img height="300px" width="300px" src="${gefilterteOutfits[index].bild}"></img>
+      <p>${gefilterteOutfits[index].beschreibung}</p>
+    </div>`;
+  }
+};
+
+window.sucheErgebnisse();
+
+window.wieIstDasWetter = function () {
+  var stadt = document.querySelector(".Stadt").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${stadt}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+  if (stadt !== "") {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        var temperatur = data.main.temp.toString().replace(".", ",");
+        var wetter = data.weather[0].main;
+        var icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        var TemperaturAnzeige = document.querySelector("#Temperatur");
+        TemperaturAnzeige.innerText = temperatur + "°C";
+
+        var iconAnzeige = document.querySelector(".Icon");
+        iconAnzeige.innerHTML = `<img src="${icon}"></img>`;
+      });
+  } else {
+    console.log("Stadt ist leer");
+  }
+};
+
+window.switchVideo = function (selectElement) {
+  var Video = document.querySelector("#myVideo");
+  var source = document.querySelector("#videoSource");
+  var jahreszeit = selectElement.value;
+  Video.pause();
+  source.src = `https://github.com/Interhyp-Praktikum/Team-1/blob/main/${jahreszeit}.mp4?raw=true`;
+  Video.load();
+  Video.play();
 };
